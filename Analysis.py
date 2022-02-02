64c1806f7e286fd1fc69b4fa82a959f036cc5510
Eytan Tsytkin <Eytan.scott@gmail.com>
2022-02-02 20:36:25 +0200

    Added updateTxValue,switched to Tx Value over time

--- Analysis.py
+++ Analysis.py
@@ -1,3 +1,4 @@
+import os
 import time
 import json
 import blocksci
@@ -27,20 +28,36 @@ def BTCtoUSD(btc,time):
 
 
 ### MUST BE FIXED - we need to
-def address_Vector(wallet):
+def AddressVector(wallet):
     # Returns a time series of wallet balance in USD
     # Each timestamp is a tx
     # 1 = wallet receives money (it is an output of the tx)
     # -1 = wallet sends money (it is an input of the tx)
-    out_list = [(1,
-                 cc.btc_to_currency(wallet.balance(tx.block_height) * SATOSHI,tx.block_time),
-                 timeToUnix(tx.block_time))for tx in wallet.output_txes]
-    in_list = [(-1,
-                cc.btc_to_currency(wallet.balance(tx.block_height) * SATOSHI,tx.block_time),
-                timeToUnix(tx.block_time)) for tx in wallet.input_txes]
+    out_list = [[1,
+                 wallet.balance(tx.block_height) * SATOSHI,
+                 tx.block_time] for tx in wallet.output_txes]
+    in_list = [[-1,
+                wallet.balance(tx.block_height) * SATOSHI,
+                tx.block_time] for tx in wallet.input_txes]
     res = sorted(out_list + in_list,key=lambda x: x[2])
+    res = updateTxValue(res)
     res = pd.DataFrame(res,columns=["type","value","time"])
     return res
+
+
+def updateTxValue(tx_list):
+    if len(tx_list) == 0:
+        pass
+    elif len(tx_list) == 1:
+        tx_list[0][1] = cc.btc_to_currency(tx_list[0][1],tx_list[0][2])
+        tx_list[0][2] = timeToUnix(tx_list[0][2])
+    elif len(tx_list) > 1:
+        balance_list = list(tx_list[idx][1] - tx_list[idx-1][1] for idx in range(1,len(tx_list)))
+        for idx in range(1,len(tx_list)):
+            tx_list[idx][1] = cc.btc_to_currency(balance_list[idx-1], tx_list[idx][2])
+            tx_list[idx][2] = timeToUnix(tx_list[idx][2])
+        tx_list[0][2] = timeToUnix(tx_list[0][2])
+    return tx_list
 
 
 def VT_VecScore(VTVec):
@@ -56,16 +73,15 @@ def plotValueTimeSeries(address,timeSeries,size,save=False,type=None):
                           cmap='coolwarm',
                           s=size)
     if type:
-        plt.title(f'Value over time in {address+type}')
+        plt.title(f'Tx over time in {type}')
+        plt.gca().add_artist(plt.legend([address],loc=4))
     else:
-        plt.title(f'Value over time in {address}')
+        plt.title(f'Tx over time in {address}')
     plt.xlabel('Time')
-    plt.ylabel('Value USD')
+    plt.ylabel('Tx Value USD')
     plt.legend(handles=scatter.legend_elements()[0],labels=['Input','Output'])
     if save:
-        filename = f'{PLOTS_PATH}VOT_{address}.png'
-        if type:
-            filename = f'{PLOTS_PATH}VOT_{address+type}.png'
+        filename = f'{PLOTS_PATH}AV_{address}.png'
         plt.savefig(filename)
     else:
         plt.show()
@@ -87,4 +103,4 @@ def checkAddress(blockchain,wallet):
 
 
 def makeVectorBatch(address_list):
-    return {wallet:address_Vector(chain.address_from_string(wallet)) for wallet in address_list if checkAddress(chain,wallet)}
+    return {wallet:AddressVector(chain.address_from_string(wallet)) for wallet in address_list if checkAddress(chain,wallet)}
--- addressbook.py
+++ addressbook.py
@@ -1,6 +1,6 @@
 import json
 
 def addressBook():
-    with open('/mnt/address_tags/AddressBook.json','r') as i:
+    with open('/root/address_book/AddressBook.json','r') as i:
         address_book = json.loads(i.read())
     return address_book
