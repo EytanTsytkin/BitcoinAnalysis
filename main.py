3c38ae33382dad74eabee9826ffd2c07ae57d4de
EytanTsytkin <Eytan.scott@gmail.com>
2022-02-14 19:31:55 +0200

    Changing update block to list comprehansion

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class AddressBook:
     def __init__(self):
         self.address_book: dict = self.load_book()
         self.update_addresses = None
-        self.cc = cc = blocksci.currency.CurrencyConverter(currency='USD',
+        self.cc = blocksci.currency.CurrencyConverter(currency='USD',
                                         start=datetime.date(2009,1,3),
                                          end=datetime.date(2021,1,31))
 
@@ -35,43 +35,81 @@ class AddressBook:
     def update_block(self, block: blocksci.Block, save = False):
         print(f'Reading block: {block.hash.__str__()}',end='\r')
         for idx, tx in enumerate(block.txes.to_list()):
-            ins,outs = self._tx_to_address_list(tx)
-            for lst in [ins,outs]:
-                for indx,address in enumerate(lst):
-                    if address[0] in self.update_addresses:
-                        print(f'Found {address[0]} in tx no.{idx}')
-                        self.address_book[address[0]]['wallet_vector'] = self.address_book[address[0]]['wallet_vector'].append({'type':address[1],
-                                        'valueBTC':self.get_value(tx,address[1],indx),
-                                        'valueUSD':0,
-                                        'time':tx.block_time},
-                                        ignore_index=True)
+            ins_outs_list = self.tx_to_address_list(tx)
+            [self.write_tx(add[0],add[1],add[2],idx,tx) for add in ins_outs_list]
+            #for address_tuple in ins_outs_list:
+                    # self.write_tx(address[0],address[1],indx,idx,tx)
+                    # print(f'Found {address[0]} in tx no.{idx}')
+                    # self.address_book[address[0]]['wallet_vector'] = self.address_book[address[0]]['wallet_vector'].append({'type':address[1],
+                    #                 'valueBTC':self.get_value(tx,address[1],indx),
+                    #                 'valueUSD':0,
+                    #                 'time':tx.block_time},
+                    #                 ignore_index=True)
 
+
+    def write_tx(self, address: str,tx_type: int, address_idx_in_tx: int, tx_idx_in_block: int ,tx: blocksci.Tx):
+        """
+        :param address: Bitcoin Wallet address
+        :param tx_type: Input / Output , position of wallet in the tx (±1)
+        :param address_idx_in_tx: index of address in the current transcation,
+               used to extract the amount being transacted to/from this address.
+        :param tx_idx_in_block: index of the tx in the block.
+        :param tx: the Tx object.
+        :return: no return - updates the
+        """
+        print(f'Found {address} in tx no.{tx_idx_in_block}', end='\r')
+        self.address_book[address]['wallet_vector'] = self.address_book[address]['wallet_vector'].append(
+            {'type': tx_type,
+             'valueBTC': self.get_value(tx, tx_type, address_idx_in_tx),
+             'valueUSD': 0,
+             'time': tx.block_time},
+            ignore_index=True)
 
 
     def get_value(self,tx: blocksci.Tx ,type:int, index: int):
+        """
+        :param tx: Tx object
+        :param type: input / output, ±1
+        :param index: index of wallet in this tx.
+        :return: The amount transacted in/out of this wallet.
+        """
         if type ==  -1:
             return tx.ins.value[index] * SATOSHI
         elif type == 1:
             return tx.outs.value[index] * SATOSHI
 
 
-    @staticmethod
-    def _tx_to_address_list(tx):
-        ins_list = []
-        outs_list = []
+    def tx_to_address_list(self,tx: blocksci.Tx):
+        """
+        :param tx: Blocksci tx
+        :return: list of tuples of 3: address,
+                                      type (±1)
+                                      index in tx (used for value extraction)
+
+        """
+        # ins_list = []
+        # outs_list = []
+        # if hasattr(tx.ins.address, 'to_list'):
+        #     for address in tx.ins.address.to_list():
+        #         if hasattr(address, 'address_string'):
+        #             ins_list.append(address.address_string)
+        # if hasattr(tx.outs.address, 'to_list'):
+        #     for address in tx.outs.address.to_list():
+        #         if hasattr(address, 'address_string'):
+        #             outs_list.append(address.address_string)
         if hasattr(tx.ins.address, 'to_list'):
-            for address in tx.ins.address.to_list():
-                if hasattr(address, 'address_string'):
-                    ins_list.append(address.address_string)
-        if hasattr(tx.outs.address, 'to_list'):
-            for address in tx.outs.address.to_list():
-                if hasattr(address, 'address_string'):
-                    outs_list.append(address.address_string)
-
-        return zip(ins_list,[-1 for i in ins_list])  , zip(outs_list,[1 for o in outs_list])
+            ins_list = [(address.address_string,1,in_idx) for in_idx, address in enumerate(tx.ins.address.to_list())
+                        if (hasattr(address, 'address_string') and address.address_string in self.update_addresses)]
+            outs_list = [(address.address_string,-1,out_idx) for out_idx, address in enumerate(tx.outs.address.to_list())
+                         if (hasattr(address, 'address_string') and address.address_string in self.update_addresses)]
+        return ins_list+outs_list
 
 
-    def updateWalletVector(self,wallet_vector):
+    def updateWalletVector(self,wallet_vector: pandas.DataFrame):
+        """
+        :param wallet_vector: A pandas dataframe holding the timeseries of a wallet.
+        :return: no return. updates the ValueUSD column.
+        """
         if len(wallet_vector) == 0:
             pass
         elif len(wallet_vector) == 1:
