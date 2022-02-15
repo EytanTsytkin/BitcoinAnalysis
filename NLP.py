de2f9c95bf52b72d1315e3fd70a71c585fc584c4
elkysandor <elky.sandor@mail.huji.ac.il>
2022-02-15 15:40:36 +0200

    bitcoin abuse changes

--- NLP.py
+++ NLP.py
@@ -1,11 +1,13 @@
 import os
+import spacy
 import nltk
 import string
 import pandas as pd
 from nltk.corpus import stopwords
 from collections import defaultdict
 from PATHS import *
-from gensim import corpora,similarities,models
+from gensim import corpora, similarities, models, utils
+from gensim.models.phrases import ENGLISH_CONNECTOR_WORDS
 import json
 from nltk.stem import PorterStemmer
 
@@ -16,6 +18,7 @@ nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
 
+nlp = spacy.load("en_core_web_sm",disable=['parser', 'ner'])
 frequency_dict = defaultdict(int)
 abuse_df = pd.read_csv(ABUSE_PATH)
 abuse_df.description = abuse_df.description.astype("string")
@@ -41,19 +44,45 @@ def clean_tokens(list_of_tokens):
     return filtered_list
 
 
-def remove_rare_words(list_of_tokens,n):
-    list_of_tokens = [word for word in list_of_tokens if frequency_dict[word] > n]
-    return list_of_tokens
+def bigrams(words, bi_min=3, tre_min=10):
+    bigram = models.Phrases(words, min_count=bi_min,threshold=tre_min,connector_words=ENGLISH_CONNECTOR_WORDS)
+    bigram_mod = models.phrases.FrozenPhrases(bigram)
+    return bigram_mod
 
 
-def create_tokens(series):
+def lemmatization(sent, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    doc = nlp(" ".join(sent))
+    texts_out = [token.lemma_ for token in doc if token.pos_ in allowed_postags]
+    return texts_out
+
+
+# def remove_rare_words(list_of_tokens,n):
+#     list_of_tokens = [word for word in list_of_tokens if frequency_dict[word] > n]
+#     return list_of_tokens
+
+
+def comput_bigram_mod(sentence,bigram_model):
+    return bigram_model[sentence]
+
+
+def create_corpus(series):
     str_series = series.str.lower()
-    print("Converted to strings..\r")
-    token_series = str_series.apply(nltk.word_tokenize)
     print("Tokenizing..\r")
-    filtered_token_series = token_series.apply(clean_tokens)
+    token_series = str_series.apply(utils.simple_preprocess,deacc=True)
     print("Filetring..\r")
-    return filtered_token_series
+    filtered_token_series = token_series.apply(clean_tokens)
+    print("create bigram model & execute..\r")
+    bigram_mod = bigrams(filtered_token_series)
+    bigram = filtered_token_series.apply(comput_bigram_mod, bigram_model=bigram_mod)
+    print("lemmatized the bigrams..\r")
+    lemmatized_bigram = bigram.apply(lemmatization)
+    print("create corpora.Dictionary..\r")
+    id2word = corpora.Dictionary(lemmatized_bigram)
+    id2word.filter_extremes(no_below=5, no_above=0.85)
+    id2word.compactify()
+    print("create corpus..\r")
+    corpus = [id2word.doc2bow(text) for text in bigram]
+    return corpus, id2word, bigram
 # the name is english_token
 
 # optional main
@@ -65,14 +94,18 @@ def create_tokens(series):
 def save_processed_corups_and_freq_dict_in_english():
     print("Making tokens..\r")
     english_abuse = select_language_with_iso639_1("en", abuse_df)
-    corpus_series = create_tokens(english_abuse.description)
-
-    print(f"Saved. Total size {os.path.getsize(ABUSE_CORPUS_PATH)}\r")
-
-
-def lda_npl_algo(final_sent_tokens):
-    dictionary = corpora.Dictionary(final_sent_tokens)
-    corpus = [dictionary.doc2bow(text) for text in final_sent_tokens]
-    #to save corpus corpora.MmCorpus.serialize('/root/abuse_data/abuse_corpus.mm', corpus)
-    model = models.LdaModel(corpus, id2word=dictionary, num_topics=20)
-    index = similarities.MatrixSimilarity(model[corpus])
+    corpus = create_corpus(english_abuse.description)
+    print("save corpus to disk..\r")
+    corpora.MmCorpus.serialize(ABUSE_CORPUS_PATH, corpus)
+    print("save freq_dict in english to disk")
+    with open(FREQ_DICT_EN,'w') as i:
+        json.dump(frequency_dict, i)
+#     print(f"Saved. Total size {os.path.getsize(ABUSE_CORPUS_PATH)}\r")
+#
+#
+# def lda_npl_algo(final_sent_tokens):
+#     dictionary = corpora.Dictionary(final_sent_tokens)
+#     corpus = [dictionary.doc2bow(text) for text in final_sent_tokens]
+#     #to save corpus corpora.MmCorpus.serialize('/root/abuse_data/abuse_corpus.mm', corpus)
+#     model = models.LdaModel(corpus, id2word=dictionary, num_topics=20)
+#     index = similarities.MatrixSimilarity(model[corpus])
